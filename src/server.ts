4b797267de1667f4995582b208f2f8d0f1a60425
armit4ge <kelvinquida24@gmail.com>
2022-11-01 09:06:42 -0400

    back-end setted up

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
+import cors from "@fastify/cors";
 
+const prisma = new PrismaClient ({
+    log:['query'],
+})
 
-async function bootstrao() {
+async function bootstrap() {
     const fastify = Fastify({
         logger:true
     })
+
+    await fastify.register(cors,{
+        origin:true
+    })
     
-    fastify.get('/pools/count', () => {
-        return {count:28176278162}
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        return {count}
     }) 
     
-    await fastify.listen({port:3333})
+    await fastify.listen({port:3333,host:'0.0.0.0'})
 }
 
-bootstrao()
+bootstrap()
