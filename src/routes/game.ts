80f1216984c44be5f3827cf7358602a8b496c4f6
Kazz2433 <kelvinquida24@gmail.com>
2022-11-22 21:43:54 -0400

    listing games within a pool

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,13 +1,57 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 
 export async function gameRoutes(fastify:FastifyInstance){
+    
+    //GAME COUNTER
     fastify.get('/game/count', async () => {
         const count = await prisma.game.count()
 
         return {count}
     }) 
+
+    //LIST GAMES WITHIN A POOL
+    fastify.get('/pools/:id/games',{
+        onRequest:[authenticate],
+    },async (req) => {
+        const getPoolParams = z.object({
+            id:z.string()
+        })
+
+        //taking an id to make sure user dont make two guesses at the same pool
+        const {id} = getPoolParams.parse(req.params)
+
+
+        const games = await prisma.game.findMany({
+            orderBy:{
+                date:'desc'
+            },
+            include:{
+                guesses:{
+                    where:{
+                        participant:{
+                            userId:req.user.sub,
+                            poolId:id
+                        }
+                    }
+                }
+            }
+        })
+
+        //returning just one guess from a user
+        return {
+            games: games.map(game => {
+                return {
+                    ...game,
+                    guess: game.guesses.length > 0 ? game.guesses[0] : null,
+                    guesses:undefined
+                }
+            })
+        }
+    })
 }
 
 
